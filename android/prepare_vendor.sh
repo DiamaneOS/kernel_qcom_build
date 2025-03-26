ba52b4c2bca846af30698fa43f72fb703da5fa24
tianwen.zhang <tianwen.zhang@t2mobile.com>
2025-03-26 18:10:19 +0800

    FPS-1158 [SW REQ] The information is wrong in bootloader UI[3]
    * Root Cause
      development require
    * Solution
      modify some info
    * Test Steps
      NA
    * Test Result
      pass
    
    Change-Id: Ifabadb1ccf3256985df0a8aa375ee685cecb20c6

--- android/prepare_vendor.sh
+++ android/prepare_vendor.sh
@@ -320,6 +320,7 @@ if [ "${RECOMPILE_ABL}" == "1" ] && [ -n "${TARGET_BUILD_VARIANT}" ] && \
       cd "${ROOT_DIR}"
 
       ./tools/bazel run \
+        --"//bootable/bootloader/edk2:build_id=${BUILD_ID}" \
         --"//bootable/bootloader/edk2:target_build_variant=${TARGET_BUILD_VARIANT}" \
         "//msm-kernel:${KERNEL_TARGET}_${KERNEL_VARIANT}_abl_dist" \
         -- --destdir "${ANDROID_KP_OUT_DIR}/dist"
